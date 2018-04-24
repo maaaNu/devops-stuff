3098cba4efc5712b01386c72804eb0d745938c80
maaaNu <hr.kniep@googlemail.com>
2018-04-24 17:51:10 +0200

    fix typo

--- install_kibana.groovy
+++ install_kibana.groovy
@@ -11,8 +11,8 @@ pipeline {
                     sh 'ansible es -m file -a "path=~/install_tmp/ state=directory"'
                     sh 'ansible es -m get_url "-a url=https://artifacts.elastic.co/downloads/kibana/kibana-6.2.4-x86_64.rpm dest=~/install_tmp/"'
                     sh 'ansible es -m get_url "-a url=https://artifacts.elastic.co/downloads/kibana/kibana-6.2.4-x86_64.rpm.sha512 dest=~/install_tmp/"'
-                    sh 'ansible es -m shell -a "cd ~/install_tmp/;sha512sum -c kibana-6.2.4.rpm.sha512"'
-                    sh 'ansible es -m shell -a "cd ~/install_tmp/;sudo rpm --install kibana-6.2.4.rpm"'
+                    sh 'ansible es -m shell -a "cd ~/install_tmp/;sha512sum -c kibana-6.2.4-x86_64.rpm.sha512"'
+                    sh 'ansible es -m shell -a "cd ~/install_tmp/;sudo rpm --install kibana-6.2.4-x86_64.rpm"'
                     sh 'ansible es -m shell -b -a "systemctl start kibana.service"'
                 }
             }
