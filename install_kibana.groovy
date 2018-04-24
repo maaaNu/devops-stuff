1c3fda0fcc3535f27ec53d5989582f073ec65ac1
maaaNu <hr.kniep@googlemail.com>
2018-04-24 17:41:25 +0200

    added kibana install script

---
+++ install_kibana.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+        stages {
+            stage('Install: jdk') {
+                steps {
+                    sh 'ansible es -b -m yum -a "name=java-1.8.0-openjdk state=installed"'
+                }
+            }
+            stage('Download: kibana'){
+                steps {
+                    sh 'ansible es -m file -a "path=~/install_tmp/ state=directory"'
+                    sh 'ansible es -m get_url "-a url=https://artifacts.elastic.co/downloads/kibana/kibana-6.2.4-x86_64.rpm dest=~/install_tmp/"'
+                    sh 'ansible es -m get_url "-a url=https://artifacts.elastic.co/downloads/kibana/kibana-6.2.4-x86_64.rpm.sha512 dest=~/install_tmp/"'
+                    sh 'ansible es -m shell -a "cd ~/install_tmp/;sha512sum -c kibana-6.2.4.rpm.sha512"'
+                    sh 'ansible es -m shell -a "cd ~/install_tmp/;sudo rpm --install kibana-6.2.4.rpm"'
+                    sh 'ansible es -m shell -b -a "systemctl start kibana.service"'
+                }
+            }
+        }
+}
