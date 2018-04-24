1abf0e23bd3e6e50521ac227a6cf8bdb89e7236e
maaaNu <hr.kniep@googlemail.com>
2018-04-24 16:33:54 +0200

    elasticsearch script

---
+++ install_elasticsearch.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+        stages {
+            stage('Install: jdk') {
+                steps {
+                    sh 'ansible all -b -m yum -a "name=java-1.8.0-openjdk state=installed"'
+                }
+            }
+            stage('Install: shasum') {
+                steps{
+                    sh 'ansible es -b -m yum -a "name=shasum state=installed"'
+                }
+            }
+            stage('Download: elasticsearch'){
+                steps {
+                    sh 'ansible es -m file -a "path=~/install_tmp/ state=directory"'
+                    sh 'ansible es -m get_url "-a url=https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.rpm dest=~/install_tmp/"'
+                    sh 'ansible es -m get_url "-a url=https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.rpm.sha512 dest=~/install_tmp/"'
+                    sh 'ansible es -m shell -a "cd ~/install_tmp/;sha512sum -c elasticsearch-6.2.4.rpm.sha512"'
+                    sh 'ansible es -m shell -a "cd ~/install_tmp/;rpm --install elasticsearch-6.2.4.rpm"'
+                    sh 'ansible es -m shell -b -a "systemctl start elasticsearch.service"'
+                }
+            }
+        }
+}
