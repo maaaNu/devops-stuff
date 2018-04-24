31b154e30cba5c8b6aa3cd67f7e43d2441301d64
U-POLARIS\mkniep <mkniep@KNIMA-LT460s.polaris.senacor.com>
2018-04-24 14:29:24 +0200

    Ansible finally working... trying out the pipeline for the first time

--- install_spring_cloud_skipper.groovy
+++ install_spring_cloud_skipper.groovy
@@ -1,9 +1,24 @@
 pipeline {
     agent any 
     stages {
-        stage('install') {
+        stage('Install: jdk') {
             steps {
-                sh 'ansible all -m ping'
+                sh 'ansible all -b -m yum -a "name=java-1.8.0-openjdk state=installed"'
+            }
+        }
+        stage('Create: directories'){
+            steps {
+                sh 'ansible all -m file -a "path=~/install_tmp/ state=directory"'
+            }
+        }
+        stage('Download: Spring-Cloud-Skipper'){
+            steps {
+                sh 'ansible all -m get_url "-a url=http://repo.spring.io/snapshot/org/springframework/cloud/spring-cloud-skipper-server/1.0.3.BUILD-SNAPSHOT/spring-cloud-skipper-server-1.0.3.BUILD-SNAPSHOT.jar dest=~/install_tmp/spring-cloud-skipper.jar"'
+            }
+        }
+        stage('Execute: Spring-Cloud-Skipper'){
+            steps {
+                sh 'ansible all -m shell -a "cd ~/install_tmp/;java -jar spring-cloud-skipper.jar"'
             }
         }
     }
